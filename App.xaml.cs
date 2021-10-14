57cefe5e75e0f77f2776856d97b0edfff89c0192
MinWG Lee <528952776@qq.com>
2021-10-14 22:14:00 +0800

    添加了全局异常处理

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,10 @@ namespace WPFCheatUITemplate
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("我们很抱歉，当前应用程序遇到一些问题，该操作已经终止，请进行重试，如果问题继续存在，请联系管理员.", "意外的操作", MessageBoxButton.OK,MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
