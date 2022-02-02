cf8129cb2b5b5bd91fadf3eb4d89dcdfbcfbddcb
MinWG Lee <528952776@qq.com>
2022-02-02 11:40:50 +0800

    添加GameMode类，用来表示游戏内模型

---
+++ GameMode/GameMode.cs
@@ -0,0 +1,40 @@
+﻿using CheatUITemplt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFCheatUITemplate.Other.Interface;
+
+namespace WPFCheatUITemplate.GameMode
+{
+    class GameMode: IAddressDatas
+    {
+        Dictionary<string, int> _data;
+
+        public GameMode()
+        {
+            _data = new Dictionary<string, int>();
+        }
+
+        virtual public void Init() { }
+       
+        virtual public void InitData() { }
+
+        public int GetAddress(string ID)
+        {
+            if (_data.ContainsKey(ID))
+            {
+                return _data[ID];
+            }else
+            {
+               var add = Other.GameFuns.AddressDataManager.GetAddress(ID);
+                _data[ID] = add;
+
+                return add;
+            }
+
+        }
+
+    }
+}
