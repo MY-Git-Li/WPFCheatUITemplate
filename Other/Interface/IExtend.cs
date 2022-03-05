998933e5516e4e72eb8e15fa9dada535a5a723d5
MinWG Lee <528952776@qq.com>
2022-03-05 12:49:59 +0800

    添加注释

--- Other/Interface/IExtend.cs
+++ Other/Interface/IExtend.cs
@@ -7,15 +7,26 @@ using System.Threading.Tasks;
 namespace WPFCheatUITemplate.Other.Interface
 {
     public interface IExtend
-    {
+    { 
+        /// <summary>
+        /// 相当于Main--异步
+        /// </summary>
         void StartAsync();
-
+        /// <summary>
+        /// 当程序关闭时--异步
+        /// </summary>
         void EndAsync();
-
+        /// <summary>
+        /// 相当于Main--同步
+        /// </summary>
         void Start();
-
+        /// <summary>
+        /// 当程序关闭时--同步
+        /// </summary>
         void End();
-
+        /// <summary>
+        /// 当游戏运行时
+        /// </summary>
         void OnGameRunAsync();
     }
 }
